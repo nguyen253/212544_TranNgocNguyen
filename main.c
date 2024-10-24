fefdd7d09f1da19b0327ca875994f36c9ea0bff9
tranngocnguyen <nguyen253@gmail.com>
2024-10-24 09:11:53 +0700

    Cau10: Add all file len server

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+// Khai báo đúng hàm từ các file .c khác
+void print_shellc1();
+void print_shellc2();
+
+int main() {
+    print_shellc1();  // Gọi hàm từ shellc1.c
+    print_shellc2();  // Gọi hàm từ shellc2.c
+    return 0;
+}
+
